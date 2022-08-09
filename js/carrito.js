08475a012cdfc1b0ac7985d6e49f84db5a3c4f81
Lucas Vasquez <lucasleonelvasquez0711@gmail.com>
2022-08-09 12:28:51 -0300

    Actualizacion y optimizacion de codigo y contenido

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,7 @@
 //Clase constructora de productos
 class Productos {
-    constructor(id, nombre, shorName, precio, categoria, descripcion, linkImg) {
+    constructor(id, nombre, precio, categoria, descripcion, linkImg) {
         this.id = parseInt(id);
-        this.shorName = shorName.toUpperCase;
         this.nombre = nombre.toUpperCase();
         this.precio = parseInt(precio);
         this.descripcion = descripcion.toUpperCase();
@@ -14,25 +13,43 @@ class Productos {
 }
 //Array combos
 const combos = [
-    new Productos(100, "Combo 1 pizza", "combo 1", 1600, "combo", "Una Clásica pizza de muzzarella + una grandefugazzeta + 2 fainas de regalo", "../img/promoDeli/Promo1.png"),
-    new Productos(101, "Combo 2 pizza", "combo 2", 1600, "combo", "Una Clásica pizza de muzzarella + una grande de jamon + 2 fainas de regalo", "../img/promoDeli/Promo2.png"),
-    new Productos(102, "Combo 3 pizza", "combo 3", 1600, "combo", "Una Clásica pizza de muzzarella + una grande napolitana + 2 fainas de regalo", "../img/promoDeli/Promo3.png")
+    new Productos(100, "Combo 1 pizza", 1600, "combo", "Una Clásica pizza de muzzarella + una grandefugazzeta + 2 fainas de regalo", "../img/promoDeli/Promo1.png"),
+    new Productos(101, "Combo 2 pizza", 1600, "combo", "Una Clásica pizza de muzzarella + una grande de jamon + 2 fainas de regalo", "../img/promoDeli/Promo2.png"),
+    new Productos(102, "Combo 3 pizza", 1600, "combo", "Una Clásica pizza de muzzarella + una grande napolitana + 2 fainas de regalo", "../img/promoDeli/Promo3.png")
 ];
 //array Pizza
 const pizza = [
-    new Productos(200, "Pizza Muzzarella", "pizza muzza", 900, "pizza", "Una Clásica pizza muzzarella con aceituna y orégano", "../img/Pizzas/pizza_muzzarela.jpg"),
-    new Productos(201, "Pizza de Jamon y Queso", "pizza JyQ", 960, "pizza", "Pizza de jamon y muzzarella con aceituna y orégano", "../img/Pizzas/pizza_jamon.jpg"),
-    new Productos(202, "Pizza Napolitana", "pizza napo", 1050, "pizza", "Pizza muzzarella con perejil , ajo , rodaja de tomate , aceituna y orégano", "../img/Pizzas/pizza_napo.jpg"),
-    new Productos(203, "Pizza Fugazzeta", "pizza fuga", 1000, "pizza", "Pizza muzzarella con cebolla ,aceituna y orégano", "../img/Pizzas/pizza_fugazzeta.jpg"),
+    new Productos(200, "Pizza Muzzarella", 900, "pizza", "Una Clásica pizza muzzarella con aceituna y orégano", "../img/Pizzas/pizza_muzzarela.jpg"),
+    new Productos(201, "Pizza de Jamon y Queso", 960, "pizza", "Pizza de jamon y muzzarella con aceituna y orégano", "../img/Pizzas/pizza_jamon.jpg"),
+    new Productos(202, "Pizza Napolitana", 1050, "pizza", "Pizza muzzarella con perejil , ajo , rodaja de tomate , aceituna y orégano", "../img/Pizzas/pizza_napo.jpg"),
+    new Productos(203, "Pizza Fugazzeta", 1000, "pizza", "Pizza muzzarella con cebolla ,aceituna y orégano", "../img/Pizzas/pizza_fugazzeta.jpg"),
+    new Productos(204,"Pizza de peperoni",1150,"pizza","Clásica pizza muzzarella con rodajas de peperoni","../img/Pizzas/pizza_peperoni.png"),
+    new Productos(205,"Pizza de especial de la casa",1300,"pizza","Pizza muzza con morrón,jamón, huevo, queso rallado, aceituna y orégano","../img/Pizzas/pizza_especialidad_casa.jpg")
 ];
 //Array Empanadas
 const empanada = [
-    new Productos(300, "Empanada de Carne", "emp Carne", 180, "empanada", "Clásica empanada de carne molida, huevo, arvejas, morrón", "../img/Empanadas/empanadas_carne.jpg"),
-    new Productos(301, "Empanada de Jamon y Queso", "emp JyQ", 180, "empanada", "Clásica empanada de jamón y muzzarella derretida al horno", "../img/Empanadas/empanadas_jyq.jpg"),
-    new Productos(302, "Empanada de Choclo", "emp Choclo", 190, "empanada", "Empanadas de choclo molido con morrón y especias al horno", "../img/Empanadas/empanadas_humita.png")
+    new Productos(300, "Empanada de Carne", 180, "empanada", "Clásica empanada de carne molida, huevo, arvejas, morrón", "../img/Empanadas/empanadas_carne.jpg"),
+    new Productos(301, "Empanada de Jamon y Queso", 180, "empanada", "Clásica empanada de jamón y muzzarella derretida al horno", "../img/Empanadas/empanadas_jyq.jpg"),
+    new Productos(302, "Empanada de Choclo", 190, "empanada", "Empanadas de choclo molido con morrón y especias al horno", "../img/Empanadas/empanadas_humita.png"),
+    new Productos(303,"Empanadas salteñas",190,"empanada","Clásicas empanadas de carne cortada a cuchillo al estilo de Salta","../img/Empanadas/empanadas_saltenias.jpg")
+];
+//Array Minutas
+const minuta = [
+    new Productos(400,"milanesa napolitana con fritas",1200,"minuta","milanesa con jamón y muzza con guarnición de papas fritas","../img/Minutas/milanesa_fritas.jpg"),
+    new Productos(401,"milanesa napolitana con pure",1150,"minuta","milanesa al horno con guarnición de pure","../img/Minutas/milanes_pure.jpg"),
+    new Productos(402,"Sándwich de milanesa completo",1250,"minuta","Sándwich de milanesa con tomate, lechuga, jamón y queso, con guarnición de fritas","../img/Minutas/sandwich_milanesaC.jpg"),
+    new Productos(403,"hamburguesa completa",1250,"minuta","hamburguesa con tomate, lechuga, cebolla, morada, jamón y queso chédar,con guarnición de fritas","../img/Minutas/hamburguesa_fritas.jpg")
+];
+//Array Bebidas 
+const bebida = [
+    new Productos(500,"CocaCola 1.5L",300,"bebida"," ","../img/Bebidas/CocaCola.webp"),
+    new Productos(501,"Pepsi 1.5L",280,"bebida"," ","../img/Bebidas/Pepsi.jpg"),
+    new Productos(502,"Cerveza Salta Rubia 1L",350,"bebida"," ","../img/Bebidas/SaltaRubia.png" ),
+    new Productos(503,"Cerveza brahama 1L",350,"bebida","","../img/Bebidas/Brahama.webp")
+
 ];
 //inventario
-const inventario = [...combos, ...pizza, ...empanada];
+const inventario = [...combos, ...pizza, ...empanada,...minuta,...bebida];
 //carrito
 let carrito = [];
 //Crear las section 
@@ -40,8 +57,9 @@ function crearhtml() {
     crearSection("Combos", "promos", combos);
     crearSection("Pizzas", "pizza", pizza);
     crearSection("Empanadas", "empanada", empanada);
+    crearSection("Minutas", "minuta", minuta);
+    crearSection("Bebidas", "bebida", bebida);
 }
-
 
 window.onload = crearhtml();
 //Crea una section
@@ -61,20 +79,20 @@ function crearSection(subTitle, id, listaProductos) {
     divRow.innerHTML = crearProductos(listaProductos);
 }
 
-
 //crea las cards de los productos
 function crearProductos(listaProductos) {
     let string = "";
     for (let i = 0; i < listaProductos.length; i++) {
+        let { id, nombre, precio, descripcion: descrip, categoria, linkImg: img } = listaProductos[i];
         string += `<div class="col-md-6 col-xxl-4">
-        <div class="producto ${listaProductos[i].categoria}" data-bs-toggle="modal" data-bs-target="#modal" value="rosa" id="${listaProductos[i].id}">
+        <div class="producto ${categoria}" data-bs-toggle="modal" data-bs-target="#modal" value="rosa" id="${id}">
             <div>
-                <img src="${listaProductos[i].linkImg}" alt="${listaProductos[i].nombre}">
+                <img src="${img}" alt="${nombre}">
             </div>
             <div>
-                <h3>${listaProductos[i].nombre}</h3>
-                <p class="descrip">${listaProductos[i].descripcion}</p>
-                <p>${listaProductos[i].precio}$</p>
+                <h3>${nombre}</h3>
+                <p class="descrip">${descrip}</p>
+                <p>${precio}$</p>
             </div>
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -100,7 +118,6 @@ cardsProductos.forEach(item => {
     })
 })
 
-
 //Modifica al modal con los atributos del producto
 function editModal(producto) {
     let canti = document.getElementById("cont_Numb");
@@ -112,18 +129,16 @@ function editModal(producto) {
     }
 }
 
-
 //Modifica el modal en base al objeto producto
 function modiModal(producto, canti) {
-    let editModal = document.getElementsByClassName("editModal");
-    editModal[0].innerText = producto.nombre;
-    editModal[1].src = producto.linkImg;
-    editModal[1].alt = producto.nombre;
-    editModal[2].innerText = producto.descripcion;
-    editModal[3].innerText = producto.precio + "$";
+    let [titulo, img, descrip, precio] = document.getElementsByClassName("editModal");
+    titulo.innerText = producto.nombre;
+    img.src = producto.linkImg;
+    img.alt = producto.nombre;
+    descrip.innerText = producto.descripcion;
+    precio.innerText = producto.precio + "$";
     canti.innerText = 1;
 }
-
 
 //evento botones de cantidad del modal y crea una advertencia
 function btnsModCant(canti) {
@@ -138,14 +153,9 @@ function btnsModCant(canti) {
     }
     //Evento del boton de restar cantidad en el modal
     lbutton.onclick = () => {
-        if (parseInt(canti.textContent) > 1) {
-            canti.innerHTML = `${parseInt(canti.textContent) - 1}`
-        } else {
-            adv.innerHTML = `No se puede ingresar menos productos`
-        }
+        parseInt(canti.textContent) > 1 ? canti.innerHTML = `${parseInt(canti.textContent) - 1}` : adv.innerHTML = `No se puede ingresar menos productos`;
     }
 }
-
 
 //Añade el producto al carrito y llama a la funcion showCarrito
 function btnAddCarrito(producto, canti) {
@@ -158,7 +168,6 @@ function btnAddCarrito(producto, canti) {
     showCarrito();
 }
 
-
 //añade al array carrito y verifica si existe o no en el array
 function añadirCarrito(producto, canti) {
     let prodToAdd = carrito.find((element) => element.id == producto.id)
@@ -168,20 +177,15 @@ function añadirCarrito(producto, canti) {
     } else {
         prodToAdd.cantidad += canti;
     }
-
 }
+//Guarda el array carrito en sessionStorage
 function saveSessionStorage() {
     sessionStorage.setItem("carrito", JSON.stringify(carrito))
 }
+//Recupera el array carrito de sessionStorage
 function getSessionStorage() {
     let carritoStorage = JSON.parse(sessionStorage.getItem("carrito"));
-    console.log(carritoStorage);
-    if (carritoStorage == null) {
-        return false;
-    } else {
-        carrito.push(...carritoStorage);
-        console.log(carrito);
-    }
+    carritoStorage == null ? false : carrito.push(...carritoStorage);
 }
 //Les da un evento a cada boton de los elementos
 function btnsCarrito() {
@@ -190,42 +194,34 @@ function btnsCarrito() {
     let labelCant = document.getElementsByClassName("cant-prodBuy");
     const btnDel = document.querySelectorAll(".dBtnBuy");
     const precioLabel = document.querySelectorAll(".precioLabel");
-
     //Btn de Sumar cantidad a un producto
     rBtnBuy.forEach(item => {
         item.addEventListener('click', () => {
             let arrayClasname = item.className.split(" ");;
             let index = parseInt(arrayClasname[2]);
+            let { cantidad, precio } = carrito[index];
+            cantidad >= 1 && btnDel[index].classList.replace("d-block", "d-none");
+            cantidad >= 1 && lBtnBuy[index].classList.replace("d-none", "d-block");
             carrito[index].cantidad++;
-            let valorCanti = carrito[index].cantidad;
-            labelCant[index].innerHTML = `${valorCanti}`
-            if (valorCanti >= 1) {
-                btnDel[index].classList.replace("d-block", "d-none");
-                lBtnBuy[index].classList.replace("d-none", "d-block");
-            }
-            precioLabel[index].innerText = `$${carrito[index].precio * carrito[index].cantidad}`
+            labelCant[index].innerHTML = `${carrito[index].cantidad}`
+            precioLabel[index].innerText = `$${precio * carrito[index].cantidad}`
             getPrecioFinal();
             saveSessionStorage();
         })
     })
-
     //Btn de restar cantidad a un producto
     lBtnBuy.forEach(item => {
         item.addEventListener('click', () => {
             let arrayClasname = item.className.split(" ");;
             let index = parseInt(arrayClasname[2]);
-            let valorCanti = carrito[index].cantidad;
-            if (carrito[index].cantidad >= 1) {
-                if (valorCanti > 2) {
-                    labelCant[index].innerHTML = `${valorCanti - 1}`;
-                } else {
-                    labelCant[index].innerHTML = `${valorCanti - 1}`;
-                    btnDel[index].classList.replace("d-none", "d-block");
-                    item.classList.replace("d-block", "d-none");
-                }
+            let { cantidad, precio } = carrito[index];
+            if (cantidad >= 1) {
+                cantidad <= 2 && btnDel[index].classList.replace("d-none", "d-block");
+                cantidad <= 2 && item.classList.replace("d-block", "d-none");
                 carrito[index].cantidad--;
+                labelCant[index].innerHTML = `${carrito[index].cantidad}`;
+                precioLabel[index].innerText = `$${precio * carrito[index].cantidad}`;
             }
-            precioLabel[index].innerText = `$${carrito[index].precio * carrito[index].cantidad}`;
             getPrecioFinal();
             saveSessionStorage();
         })
@@ -238,17 +234,15 @@ function btnsCarrito() {
             let index = parseInt(arrayClasname[2]);
             carrito.splice(index, 1);
             showCarrito()
+            let canasta = document.getElementById("carrito");
             if (carrito.length == 0) {
-                let canasta = document.getElementById("carrito");
-                canasta.innerHTML = `<div class="d-block msgSinP" >
-                <h3>No hay productos</h3>
-            </div>
-            `
+                canasta.innerHTML = `<div class="d-block msgSinP"><h3>No hay productos</h3></div>`
             }
             saveSessionStorage();
         })
     })
 }
+//Evento para cargar el sessionStorage
 window.onload = () => {
     getSessionStorage();
     if (carrito.length != 0) {
@@ -258,17 +252,12 @@ window.onload = () => {
 
 //muestra los productos y actualiza cuando se borra uno
 function showCarrito() {
-
     let canasta = document.getElementById("carrito");
     let prodCarrito = "";
     for (let i = 0; i < carrito.length; i++) {
-        let classBtndl = "d-none";
-        let classLbtn = "d-block";
         //Verifica si la cantidad es uno para mostrar el btn de borrar
-        if (carrito[i].cantidad == 1) {
-            classBtndl = "d-block";
-            classLbtn = "d-none";
-        }
+        let classBtndl = carrito[i].cantidad == 1 ? "d-block" : "d-none";
+        let classLbtn = carrito[i].cantidad == 1 ? "d-none" : "d-block";
         //Genera el string con cada elemento
         prodCarrito += `<div class="prodCarrito" >
                             <div>
@@ -324,36 +313,25 @@ function getPrecioFinal() {
     document.getElementById("precioFinal").innerHTML = "$" + precioFinal;
 }
 
+//boton para finalizar el pedido
 const btnFinal = document.getElementById("btnFinalizar");
 btnFinal.onclick = () => {
-    if(carrito.length != 0){
-        alert("Su pedido ha sido enviado");
-    }
-    carrito.splice(0);
+    carrito.length != 0 && alert("Su pedido ha sido enviado");
     showCarrito();
-    if (carrito.length == 0) {
-        let canasta = document.getElementById("carrito");
-        canasta.innerHTML = `<div class="d-block msgSinP" >
-                <h3>No hay productos</h3>
-            </div>
-            `
-    }
+    resetCarrito();
     saveSessionStorage();
 }
+//boton para borrar el pedido
 const borra = document.getElementById("borrar");
 borra.onclick = () => {
-    if(carrito.length != 0){
-        alert("Su pedido ha sido borrado");
-    }
-    
+    carrito.length != 0 && alert("Su pedido ha sido borrado");
+    resetCarrito();
+    saveSessionStorage();
+}
+//funcion para limpiar el carrito
+function resetCarrito() {
     carrito.splice(0);
     showCarrito();
-    if (carrito.length == 0) {
-        let canasta = document.getElementById("carrito");
-        canasta.innerHTML = `<div class="d-block msgSinP" >
-                <h3>No hay productos</h3>
-            </div>
-            `
-    }
-    saveSessionStorage();
+    let canasta = document.getElementById("carrito");
+    canasta.innerHTML = carrito.length == 0 && `<div class="d-block msgSinP"><h3>No hay productos</h3></div>`
 }
